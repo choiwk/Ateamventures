40103dd7643988c30d485cb9a3de3a3bbd692453
dawon <lyumilove9@gmail.com>
2022-02-08 14:56:25 +0900

    [Feat] - 필터 옵션, 필터 태그 제작

---
+++ src/components/Filtering/Filter.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { filterOption } from 'utils/constants/filterOption';
+import material from 'utils/constants/material';
+import method from 'utils/constants/method';
+import { Arrow, Refresh } from 'assets/assets';
+
+import 'styles/Filter.scss';
+
+function Filter() {
+  const [methodClick, setMethodClick] = useState<boolean>(false);
+  const [materialClick, setMaterialClick] = useState<boolean>(false);
+
+  const methodHandler = (e: any) => {
+    setMethodClick(!methodClick);
+  }
+
+  const materialHandler = (e: any) => {
+    setMaterialClick(!materialClick);
+  }
+
+  const resetHandler = (e: any) => {
+    setMethodClick(false);
+    setMaterialClick(false);
+  }
+
+  return (
+    <div className='btn'>
+      <div className='method-checkbox'>
+        <span className={methodClick ? 'method-default clickBg': 'method-default'} onClick={methodHandler}>가공방식
+          <img src={Arrow} alt='drop-down-arrow' className='drop-icon'></img>
+        </span>
+        <ul className={methodClick ? 'method click' : 'method hide'}>
+          {method.map((i: filterOption) => {
+            return (
+            <li key={i.id}>
+              <div className='method-container'>
+                <input className='method-input' type='checkbox' id='method'>
+                </input>
+                <label htmlFor='method' className='custom-checkbox'>{i.value}</label>
+            </div>
+            </li>
+            
+          )
+        })}
+        </ul>
+      </div>
+      <div className={'material-checkbox'}>
+        <span className={materialClick ? 'material-default clickBg' : 'material-default'} onClick={materialHandler}>재료
+          <img src={Arrow} alt='drop-down-arrow' className='drop-icon'></img>
+        </span>
+        <ul className={materialClick ? 'material click' : 'material hide'}>
+          {material.map((i: filterOption) => {
+            return (
+            <li key={i.id}>
+              <div className='material-container'  key={i.id}>
+                <input className='material-container' type='checkbox' id='material'>
+                </input>
+                <label htmlFor='material'>{i.value}</label>
+              </div>
+            </li>
+          )
+        })}
+        </ul>
+      </div>
+      <div className={methodClick || materialClick ? 'refresh-btn click' : 'refresh-btn hide'} onClick={resetHandler}>
+        <div className='refresh-btn-img'>
+          <img src={Refresh} alt='refresh btn' className='refresh-img'></img>
+        </div>
+        <div>
+        <p className='refresh-text'>필터링 리셋</p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;

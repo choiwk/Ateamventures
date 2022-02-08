aa10bd618612c0174259da3b9b8dde77c4ad614c
dawon <lyumilove9@gmail.com>
2022-02-08 14:53:56 +0900

    [Feat] - 카드 데이터 추가, 상담중 뱃지 제작

---
+++ src/components/Requestcard/Badge.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'styles/Badge.scss';
+
+function Badge() {
+  return (
+    <div className='badge-container'>
+      <button className='badge-btn'>상담중</button>
+    </div>
+  );
+}
+
+export default Badge;

1a7d22cf620cae28c6373fbbcb1058ac3ca146f4
dawon <lyumilove9@gmail.com>
2022-02-08 14:52:11 +0900

    [Feat] - 견적 요청 카드 제작

---
+++ src/components/Requestcard/chatBtn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'styles/ChatBtn.scss';
+
+function ChatBtn() {
+  return (
+    <div className='btn-wrapper'>
+      <button className='chat-btn'>채팅하기</button>
+    </div>
+  );
+}
+
+export default ChatBtn;
---
+++ src/components/Requestcard/requestBtn.tsx
@@ -0,0 +1,11 @@
+import 'styles/RequestBtn.scss';
+
+function RequestBtn() {
+  return (
+    <div className='request-btn-wrapper'>
+      <button className='request-btn'>요청 내역 보기</button>
+    </div>
+  );
+}
+
+export default RequestBtn;

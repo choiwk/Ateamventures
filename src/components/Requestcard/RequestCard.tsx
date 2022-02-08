9b0999ff2003d59fa23ff8a658aa81fe2b5e7a35
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-02-08 19:08:18 +0900

    [Style] RequestCard server에서 받은 데이터 화면에 처리 진행 중

--- src/components/Requestcard/RequestCard.tsx
+++ src/components/Requestcard/RequestCard.tsx
@@ -10,14 +10,19 @@ const RequestCard: FC = () => {
       .then((res) => res.json())
       .then((json) => setServerData(json));
   }, []);
-
+  console.log(serverData);
   return (
     <main>
       {serverData.map((card: IRequest, index: number) => (
         <div className='prototype-card' key={index}>
-          <h2 className='prototype-title' aria-label='시제품 타이틀'>
-            {card.title}
-          </h2>
+          <div className='prototype-header'>
+            <h2 className='prototype-title' aria-label='시제품 타이틀'>
+              {card.title}
+            </h2>
+            <span className='client'>{card.client}</span>
+            <time className='due-date'>{card.due}까지 납기</time>
+          </div>
+          <div className='contour' aria-label='구분선' />
         </div>
       ))}
     </main>

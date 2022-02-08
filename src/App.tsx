2b0bcaa80b9abfb014e655ba78a54572893828c4
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-02-08 18:28:11 +0900

    [Feat] App.tsx grid 레이아웃 적용 및 컴포넌트들 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import 'styles/App.css';
+import Nav from 'components/Navbar/Nav';
+import Header from './components/Header/Header';
+import Filter from 'components/Filtering/Filter';
+import RequestCard from 'components/Requestcard/RequestCard';
 
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Nav />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <Header />
+            <Filter />
+            <RequestCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;

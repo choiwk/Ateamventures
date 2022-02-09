b0837e8b424bd491ea4becba5e97f1b2ada1a63b
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-02-10 01:22:38 +0900

    [Feat] CheckBox에서 얻은 id 값을 처리 하는 filter 기능 구현중

--- src/components/Filtering/Filter.tsx
+++ src/components/Filtering/Filter.tsx
@@ -1,11 +1,30 @@
 import React, { FC, useEffect, useState } from 'react';
 
+import material from 'utils/constants/Materials';
+import method from 'utils/constants/Methods';
+import CheckBox from './CheckBox/CheckBox';
 import '../Filtering/Filter.scss';
 
 const Filter: FC = () => {
+  const [filtersBox, setFiltersBox] = useState<any>({ method: [], material: [] });
+
+  const showFilterResults = (filters: any) => {};
+
+  const handleFilters = (filters: any, category: any) => {
+    console.log(filters);
+    const newFilters = { ...filtersBox };
+    console.log(newFilters);
+    newFilters[category] = filters;
+    if (category === 'material') {
+    }
+    showFilterResults(newFilters);
+    setFiltersBox(newFilters);
+  };
+
   return (
     <div>
-      <section>
+      <CheckBox handleFilters={handleFilters} />
+      {/* <section>
         <select name='processing' id='process-combox'>
           <option value='default' disabled>
             가공방식
@@ -23,7 +42,7 @@ const Filter: FC = () => {
           <option value='합금강'>합금강</option>
           <option value='강철'>강철</option>
         </select>
-      </section>
+      </section> */}
     </div>
   );
 };
